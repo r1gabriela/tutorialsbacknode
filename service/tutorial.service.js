12b3218ce9e2430d48b8e470457c40fd1f753e00
r1gabriela <andradegabriela20@gmail.com>
2020-10-09 12:54:19 -0300

    service, controller, gitignore

---
+++ service/tutorial.service.js
@@ -0,0 +1,32 @@
+const db = require('../model')
+const Tutorial = db.Tutorial;
+const Op = db.Sequelize.Op;
+
+
+exports.create = (req,res) => {
+
+};
+
+exports.findAll = (req,res) => {
+
+};
+
+exports.findOne = (req,res) => {
+
+};
+
+exports.update = (req,res) => {
+
+};
+
+exports.delete = (req,res) => {
+
+};
+
+exports.deleteAll = (req,res) => {
+
+};
+
+exports.findAllPublished = (req,res) => {
+
+};
